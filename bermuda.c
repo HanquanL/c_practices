4e61424d9fb58d89b2a82f21280011d5b5b71239
HanquanL <HanquanL0115@gmail.com>
2024-11-06 23:31:54 -0500

    start bermuda

---
+++ bermuda.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float latitude;
+    float longitude;
+    char info[80];
+    int started = 0;
+
+    puts("data=[");
+    
+    puts("\n]");
+    return 0;
+}
