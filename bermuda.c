146eb3b8b3e762a4106504eb346bca8579eaf0d0
HanquanL <HanquanL0115@gmail.com>
2024-11-07 21:36:42 -0500

    completed bermuda

--- bermuda.c
+++ bermuda.c
@@ -5,10 +5,10 @@ int main()
     float latitude;
     float longitude;
     char info[80];
-    int started = 0;
-
-    puts("data=[");
-    
-    puts("\n]");
+    while(scanf("%f,%f,%79[^\n]",&latitude, &longitude, info) == 3){
+        if(latitude > 26 && latitude < 34)
+            if(longitude > -76 && longitude < -64)
+                printf("%f,%f,%s}", latitude, longitude, info);
+    }
     return 0;
 }
