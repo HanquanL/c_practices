1f50f865831bbed2d38edfdbf3be54d64cb4c378
HanquanL <HanquanL0115@gmail.com>
2024-11-15 16:49:47 -0500

    improve mr_right

--- ch7/Mr_right.c
+++ ch7/Mr_right.c
@@ -11,21 +11,43 @@ char *ADS[] = {
 "Josh: SJM likes sports, movies and theater",
 "Jed: DBM likes theater, books and dining"
 };
-void find()
+void find(int (*match)(char*))
 {
     int i ;
     puts("Search results:");
     puts("------------------------------------");
     for(i = 0; i < NUM_ADS; i++){
-        if(strstr(ADS[i], "sports") && !strstr(ADS[i], "bieber")){
+        if(match(ADS[i])){
             printf("%s\n", ADS[i]);
         }
     }
     puts("------------------------------------");
 }
 
+int sports_no_bieber(char *s)
+{
+    return strstr(s, "sports") && !strstr(s, "bieber");
+}
+
+int sports_or_workout(char *s)
+{
+    return strstr(s, "sports") || strstr(s, "workout");
+}
+
+int ns_theater(char *s)
+{
+    return strstr(s, "NS" ) && strstr(s, "theater");
+}
+
+int arts_theater_or_dining(char *s){
+    return strstr(s, "arts") || strstr(s, "theater") || strstr(s, "dining");
+}
+
 int main()
 {
-    find();
+    find(sports_no_bieber);
+    find(sports_or_workout);
+    find(ns_theater);
+    find(arts_theater_or_dining);
     return 0;
 }
