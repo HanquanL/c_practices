af68c56d95047b21f3200f4badadba2985a129f3
HanquanL <HanquanL0115@gmail.com>
2024-11-14 19:35:10 -0500

    complete linked_island, start spies.c

--- ch6/linked_island.c
+++ ch6/linked_island.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct island{
     char *name;
@@ -15,18 +17,52 @@ void display(island *start)
     }
 }
 
+island* create(char *name)
+{
+    island *i = malloc(sizeof(island));
+    i -> name = strdup(name);
+    i -> opens = "09:00";
+    i -> closes = "17:00";
+    i -> next = NULL;
+    return i;
+}
+
+void release(island *start)
+{
+    island *i = start;
+    island *next = NULL;
+    for(; i != NULL; i = next){
+        next = i -> next;
+        free(i -> name);
+        free(i);
+    }
+}
+
 int main()
 {
-    island amity = {"Amity", "09:00", "17:00", NULL};
-    island craggy = {"Craggy", "09:00", "17:00", NULL};
-    island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
-    island shutter = {"Shutter", "09:00", "17:00", NULL};
-    island skull = {"Skull", "09:00", "17:00", NULL};
+    // island amity = {"Amity", "09:00", "17:00", NULL};
+    // island craggy = {"Craggy", "09:00", "17:00", NULL};
+    // island isla_nublar = {"Isla Nublar", "09:00", "17:00", NULL};
+    // island shutter = {"Shutter", "09:00", "17:00", NULL};
+    // island skull = {"Skull", "09:00", "17:00", NULL};
 
-    amity.next = &craggy;
-    craggy.next = &isla_nublar;
-    isla_nublar.next = &skull;
-    skull.next = &shutter;
-    display(&amity);
+    // amity.next = &craggy;
+    // craggy.next = &isla_nublar;
+    // isla_nublar.next = &skull;
+    // skull.next = &shutter;
+    // display(&amity);
+    island * start = NULL;
+    island * i = NULL;
+    island * next = NULL;
+    char name[80];
+    for(; fgets(name, 80, stdin); i = next){
+        next = create(name);
+        if(start ==NULL)
+            start = next;
+        if(i != NULL)
+            i -> next = next;
+    }
+    display(start);
+    release(start);
     return 0;
 }
