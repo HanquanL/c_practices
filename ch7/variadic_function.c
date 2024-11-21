9bddedc474c494627f8ca3975478cd39b489dcac
HanquanL <HanquanL0115@gmail.com>
2024-11-20 21:39:57 -0500

    completed variadic_function.c

---
+++ ch7/variadic_function.c
@@ -0,0 +1,19 @@
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_ints(int args, ...)
+{
+    va_list ap;
+    va_start(ap, args);
+    int i ;
+    for(i = 0; i < args; i++){
+        printf("argument:  %i\n", va_arg(ap, int));
+    }
+    va_end(ap);
+}
+
+int main()
+{
+    print_ints(3, 79, 101, 32);
+    return 0;
+}
