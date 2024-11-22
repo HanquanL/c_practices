bd1cd5ad08650cdb8b99e29abff25c15b357db63
HanquanL <HanquanL0115@gmail.com>
2024-11-22 16:38:39 -0500

    completed and tested timestamp.c

--- ch9/timestamp.c
+++ ch9/timestamp.c
@@ -4,7 +4,18 @@
 
 char* now()
 {
-    time_t time;
-    time(&time);
-    return asctime(localtime(&time));
+    time_t t;
+    time(&t);
+    return asctime(localtime(&t));
+}
+
+int main()
+{
+    char comment[80];
+    char cmd[120];
+
+    fgets(comment, 80, stdin);
+    sprintf(cmd, "echo '%s %s' >> reports.log", comment, now());
+    system(cmd);
+    return 0;
 }
