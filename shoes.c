5c76d41a759d02e91274a74473301f14fe821689
HanquanL <Hanquanl0115@gmail.com>
2026-03-27 19:46:35 -0400

    complete shoe

--- shoes.c
+++ shoes.c
@@ -10,7 +10,12 @@ int main (){
     shoe = 3.0;
     while(shoe < 18.5){
         foot = SCALE * shoe + ADJUST;
-        printf()
+        printf("%10.1f %15.2f inches\n",shoe ,foot);
+        shoe = shoe + 1.0;
     }
+
+    printf("If the shoe fits, wear it.\n");
+
+    return 0;
 }
 
