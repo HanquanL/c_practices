669ff4f23782ffbd67db3c300cc139d16d256f82
HanquanL <Hanquanl0115@gmail.com>
2026-04-09 09:53:58 -0400

    complete total_count_avg.py

--- python/total_count_avg.py
+++ python/total_count_avg.py
@@ -1,5 +1,13 @@
 total = 0
 count = 0
 User_input = 0
-while User_input != "done":
-    User_input = input("Enter a number")
+while True:
+    User_input = input("Enter a number: ")
+    if User_input == "done":
+        break
+    try:
+        total += int(User_input)
+        count += 1
+    except ValueError:
+        print("Invalid input")
+print(total, count, total / count)
